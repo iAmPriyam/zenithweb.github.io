510e842ddef9bf4a54a61fe600d56ca062247ee6
Aditya Priyam <apriyam9@gmail.com>
2019-01-14 10:44:37 +0530

    minor changes, readme update and title update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # iampriyam.zenithweb.io
+
+Website hosted @ https://github.com/iAmPriyam.github.io/zenithweb.github.io/
+
+!!-- The website is still under construction --!!
+
+Project mainainer and developer - Aditya Priyam
--- js/animate.js
+++ js/animate.js
@@ -4,7 +4,7 @@ var nav= $('#nav');
 function setup(){
     $(title).velocity({translateY:500},{duration:400, easing:"ease-in-out"});
     $(title).velocity({opacity:1});
-    $(nav).velocity({ width: 80 });
+    $(nav).velocity({ width: 80 },{duration:600, easing:"ease-in-out-back"});
 }
 
 window.addEventListener("load",setup,false);

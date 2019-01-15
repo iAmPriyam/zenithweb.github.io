6234d1ec5a3ad2624e17d04caf8d1a222b5177eb
Aditya Priyam <apriyam9@gmail.com>
2019-01-15 15:23:48 +0530

    Content container created, edited and grid formation of the cards done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 var title= $('#title');
+var id=$('id');
 var tcontainer = $('#tcontainer');
 $tcontainer.css('background','blue');
-// function setup(){
-//     $title.velocity({translateY:-100},{duration:400,easing:"ease-in-out"});
-//     $title.velocity({opacity:1});
-// }
+function setup(){
+var i = 0;
+var txt = 'Lorem ipsum dummy text blabla.';
+var speed = 50;
+  if (i < txt.length) {
+    $id.innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+    $title.velocity({translateY:-100},{duration:400,easing:"ease-in-out"});
+    $title.velocity({opacity:1});
+}
 
 window.addEventListener("load",setup,false);

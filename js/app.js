be9c64fd9ae9ea1a939889dc4387355fd78b9cec
Aditya Priyam <apriyam9@gmail.com>
2019-01-15 22:56:28 +0530

    Updated content container, added images and shadows to text cards

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var tcontainer = $('#tcontainer');
 $tcontainer.css('background','blue');
 function setup(){
 var i = 0;
-var txt = 'Lorem ipsum dummy text blabla.';
+var txt = 'Lorem ipsum dummy text bla bla.';
 var speed = 50;
   if (i < txt.length) {
     $id.innerHTML += txt.charAt(i);
